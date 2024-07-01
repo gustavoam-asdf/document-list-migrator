6d4875f7fe064302423a791318230cc41607c586
Gustavo A <gustavo.atencio@outlook.com>
2024-06-30 20:36:25 -0500

    extract zip data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+import { Open as OpenZip } from "unzipper";
 import { fileCompressedUrl } from "./constants";
+
+const startTime = Date.now();
 
 console.log("Downloading file...");
 const dataZipped = await fetch(fileCompressedUrl)
@@ -6,3 +9,11 @@ const dataZipped = await fetch(fileCompressedUrl)
 console.log("Writing file...");
 await Bun.write("./list.zip", dataZipped)
 
+console.log("Unzipping file...");
+const directory = await OpenZip.file("./list.zip")
+await directory.extract({ path: "./" })
+
+
+const endTime = Date.now();
+
+console.log(`Done in ${endTime - startTime}ms`);
