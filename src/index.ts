74301cbff31f0b4bc1d4309ba9d89748ebb8ac4a
Gustavo A. <gustavo.atencio@outlook.com>
2024-07-02 11:53:38 -0500

    order data

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { TextDecoderStream } from "./polifylls";
-import { filesDir, localFile } from "./constants";
 import { updateRucsFile } from "./updateRucsFile";
 
 const startTime = Date.now();
@@ -7,62 +5,10 @@ const startTime = Date.now();
 const pid = process.pid;
 console.log(`PID: ${pid}`);
 
-// await updateRucsFile();
-
-const file = Bun.file(localFile)
-const fileStream = file.stream()
-
-const decoderStream = new TextDecoderStream("latin1")
-
-class LineSplitter implements Transformer<string, string> {
-	private buffer: string;
-
-	constructor() {
-		this.buffer = '';
-	}
-
-	transform(chunk: string, controller: TransformStreamDefaultController<string>) {
-		this.buffer += chunk;
-		let lines = this.buffer.split('\n');
-		this.buffer = lines.pop() || ''; // Guardar el último fragmento para la próxima chunk
-		for (let line of lines) {
-			controller.enqueue(line);
-		}
-	}
-
-	flush(controller: TransformStreamDefaultController<string>) {
-		if (this.buffer) {
-			controller.enqueue(this.buffer);
-		}
-	}
-}
-
-const dnisFile = Bun.file(`${filesDir}/dnis.txt`);
-
-const dnisWriter = dnisFile.writer({
-	highWaterMark: 1024 * 1024 * 100,
-});
-
-const classifierStream = new WritableStream<string>({
-	write(line) {
-		const isRuc10 = line.startsWith('10');
-		if (!isRuc10) {
-			return;
-		}
-
-		dnisWriter.write(line + "\n");
-	},
-	close() {
-		dnisWriter.end();
-	}
-});
-
-const lineTransformStream = new TransformStream(new LineSplitter);
-
-await fileStream
-	.pipeThrough(decoderStream)
-	.pipeThrough(lineTransformStream)
-	.pipeTo(classifierStream);
+const {
+	dnisPath,
+	rucsPath,
+} = await updateRucsFile();
 
 const endTime = Date.now();
 
--- src/updateRucsFile.ts
+++ src/updateRucsFile.ts
@@ -1,7 +1,8 @@
-import { filesDir, localZipFile, remoteZipFile } from "./constants";
+import { filesDir, localFile, localZipFile, remoteZipFile } from "./constants";
 
 import { $ } from "bun";
 import fs from "node:fs/promises";
+import { LineSplitter } from "./transformers/LineSplitter";
 
 export async function updateRucsFile() {
 	console.log("Cleaning files directory...");
@@ -16,4 +17,41 @@ export async function updateRucsFile() {
 
 	console.log("Unzipping file...");
 	await $`unzip ${localZipFile} -d ${filesDir} > /dev/null`
+
+	const dnisFilePath = `${filesDir}/dnis.txt`
+	const dnisFile = Bun.file(dnisFilePath);
+
+	const dnisWriter = dnisFile.writer({
+		highWaterMark: 1024 * 1024 * 100,
+	});
+
+	const classifierStream = new WritableStream<string>({
+		write(line) {
+			const isRuc10 = line.startsWith('10');
+			if (!isRuc10) {
+				return;
+			}
+
+			dnisWriter.write(line + "\n");
+		},
+		close() {
+			dnisWriter.end();
+		}
+	});
+
+	const decoderStream = new TextDecoderStream("latin1")
+	const lineTransformStream = new TransformStream(new LineSplitter);
+
+	const file = Bun.file(localFile)
+	const fileStream = file.stream()
+
+	await fileStream
+		.pipeThrough(decoderStream)
+		.pipeThrough(lineTransformStream)
+		.pipeTo(classifierStream);
+
+	return {
+		dnisPath: dnisFilePath,
+		rucsPath: localFile,
+	}
 }
