847abf66324a8fd086f92abeac34ea8310540564
Gustavo A. <gustavo.atencio@outlook.com>
2024-07-03 18:48:24 -0500

    write data in sql and json format

--- script/ubigeoScrapper.ts
+++ script/ubigeoScrapper.ts
@@ -1,3 +1,4 @@
+import { filesDir } from "../src/constants";
 import puppeteer from "puppeteer";
 
 //https://prod3.seace.gob.pe/portalseace-uiwd-pub/ControllerServletOpen?pageid=0&portletid=BuscadorUbigeo&scriptdo=doView&page__ind=&v_dep_cod=01&v_pro_cod=01&v_dis_cod=01
@@ -121,8 +122,85 @@ for await (const regionWithoutProvinces of regionsWithoutProvinces) {
 	});
 }
 
-console.dir(regionsWithoutDistricts, {
-	depth: null
-})
+const regions: Region[] = []
+
+for await (const regionWithoutDistricts of regionsWithoutDistricts) {
+	const regionBaseUrl = `${trueSource}&v_dep_cod=${regionWithoutDistricts.code}`;
+
+	const districtPage = await browser.newPage();
+
+	for await (const provinceWithoutDistrict of regionWithoutDistricts.provinces) {
+		const provinceUrl = `${regionBaseUrl}&v_pro_cod=${provinceWithoutDistrict.code}`;
+
+		console.log(`Searching districts for region ${regionWithoutDistricts.value} for province ${provinceWithoutDistrict.value}`);
+		await districtPage.goto(provinceUrl);
+
+		const districts: District[] = await districtPage.evaluate(() => {
+			const $districtsSelector = document.querySelector(`select[name="v_dis_cod"]`);
+			if (!$districtsSelector) {
+				throw new Error("Districts selector not found");
+			}
+
+			const $districtOptions = $districtsSelector.querySelectorAll("option");
+			if (!$districtOptions) {
+				throw new Error("Districts options not found");
+			}
+
+			const districts = Array
+				.from($districtOptions)
+				.map($district => {
+					const districtCode = $district.getAttribute("value")!;
+
+					if (districtCode === "00") {
+						return null;
+					}
+
+					const districtValue = $district.textContent?.replace(`${districtCode}-`, "").trim()!;
+
+					const district: District = {
+						code: districtCode,
+						value: districtValue
+					}
+
+					return district;
+				})
+				.filter(Boolean) as District[];
+
+			return districts;
+		});
+
+		regions.push({
+			code: regionWithoutDistricts.code,
+			value: regionWithoutDistricts.value,
+			provinces: [
+				{
+					code: provinceWithoutDistrict.code,
+					value: provinceWithoutDistrict.value,
+					disctricts: districts
+				}
+			]
+		});
+	}
+}
+
+const jsonFile = `${filesDir}/ubigeo.json`;
+Bun.write(jsonFile, JSON.stringify(regions, null, 2));
+
+const copyFile = `${filesDir}/ubigeo.copy.sql`;
+const copyFileHeader = `COPY public."Ubigeo" (codigo, region, provincia, distrito) FROM stdin;
+\n`;
+const copyFileContent = regions.reduce((acc, region) => {
+	const regionContent = region.provinces.reduce((acc, province) => {
+		const provinceContent = province.disctricts.reduce((acc, district) => {
+			return `${acc}${region.code}${province.code}${district.code}\t${region.value}\t${province.value}\t${district.value}\n`;
+		}, "");
+
+		return `${acc}${provinceContent}`;
+	}, "");
+
+	return `${acc}${regionContent}`;
+}, "");
+
+Bun.write(copyFile, `${copyFileHeader}${copyFileContent}`);
 
 await browser.close();
