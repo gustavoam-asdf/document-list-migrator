9abe45082b0c97a6c0688d10a918133b8109516e
Gustavo A. <gustavo.atencio@outlook.com>
2024-07-03 10:27:15 -0500

    fix: change env

--- src/db.ts
+++ src/db.ts
@@ -5,6 +5,6 @@ export const sql = postgres({
 	port: Number(Bun.env.DATABASE_PORT),
 	database: Bun.env.DATABASE_NAME,
 	username: Bun.env.DATABASE_USER,
-	password: Bun.env.DATABASE_PASS,
+	password: Bun.env.DATABASE_PASSWORD,
 	ssl: true,
 }) 
