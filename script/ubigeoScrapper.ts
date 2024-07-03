e3b105c3fc2c9badc90c4f0ed28bb2e60dff2afa
Gustavo A. <gustavo.atencio@outlook.com>
2024-07-03 18:38:56 -0500

    read provinces

--- script/ubigeoScrapper.ts
+++ script/ubigeoScrapper.ts
@@ -2,7 +2,7 @@ import puppeteer from "puppeteer";
 
 //https://prod3.seace.gob.pe/portalseace-uiwd-pub/ControllerServletOpen?pageid=0&portletid=BuscadorUbigeo&scriptdo=doView&page__ind=&v_dep_cod=01&v_pro_cod=01&v_dis_cod=01
 
-const trueSource = 'https://prod3.seace.gob.pe/portalseace-uiwd-pub/ControllerServletOpen?pageid=0&portletid=BuscadorUbigeo&scriptdo=doView&page__ind=';
+const trueSource = 'https://prod3.seace.gob.pe/portalseace-uiwd-pub/ControllerServletOpen?pageid=0&portletid=BuscadorUbigeo&scriptdo=doView';
 
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
@@ -42,19 +42,9 @@ const exteriorRegion: Region = {
 	]
 }
 
-// Extract data
 const regionsWithoutProvinces = await page.evaluate(() => {
-	const $regionsSelector = document.querySelector(`select[name="v_dep_cod"]`);
-
-	if (!$regionsSelector) {
-		return null;
-	}
-
-	const $regionOptions = $regionsSelector.querySelectorAll("option");
-
-	if (!$regionOptions) {
-		return null;
-	}
+	const $regionsSelector = document.querySelector(`select[name="v_dep_cod"]`)!;
+	const $regionOptions = $regionsSelector.querySelectorAll("option")!;
 
 	const regionsWithoutProvinces = Array
 		.from($regionOptions)
@@ -79,6 +69,60 @@ const regionsWithoutProvinces = await page.evaluate(() => {
 	return regionsWithoutProvinces;
 });
 
-console.log(regionsWithoutProvinces);
+const regionsWithoutDistricts: (Omit<Region, "provinces"> & {
+	provinces: Omit<Province, "disctricts">[];
+})[] = []
+
+for await (const regionWithoutProvinces of regionsWithoutProvinces) {
+	const regionPage = await browser.newPage();
+	const regionUrl = `${trueSource}&v_dep_cod=${regionWithoutProvinces.code}`;
+
+	console.log(`Searching provinces for region ${regionWithoutProvinces.value}`);
+	await regionPage.goto(regionUrl);
+
+	const provincesWithOutDistricts: Omit<Province, "disctricts">[] = await regionPage.evaluate(() => {
+		const $provincesSelector = document.querySelector(`select[name="v_pro_cod"]`);
+		if (!$provincesSelector) {
+			throw new Error("Provinces selector not found");
+		}
+
+		const $provinceOptions = $provincesSelector.querySelectorAll("option");
+		if (!$provinceOptions) {
+			throw new Error("Provinces options not found");
+		}
+
+
+		const provinces = Array
+			.from($provinceOptions)
+			.map($province => {
+				const provinceCode = $province.getAttribute("value")!;
+
+				if (provinceCode === "00") {
+					return null;
+				}
+
+				const provinceValue = $province.textContent?.replace(`${provinceCode}-`, "").trim()!;
+
+				const province: Omit<Province, "disctricts"> = {
+					code: provinceCode,
+					value: provinceValue
+				}
+
+				return province;
+			})
+			.filter(Boolean) as Omit<Province, "disctricts">[];
+
+		return provinces;
+	});
+
+	regionsWithoutDistricts.push({
+		...regionWithoutProvinces,
+		provinces: provincesWithOutDistricts
+	});
+}
+
+console.dir(regionsWithoutDistricts, {
+	depth: null
+})
 
 await browser.close();
