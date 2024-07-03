2c432a085f93b5241b53af63c19804444a4084bd
Gustavo A <gustavo.atencio@outlook.com>
2024-07-03 09:37:47 -0500

    more logs

--- src/workers/dniWorker.ts
+++ src/workers/dniWorker.ts
@@ -1,10 +1,10 @@
-import { sql } from "../db";
-import { TextDecoderStream } from "../polifylls";
-import { splitInParts } from "../splitInParts";
 import { LineGrouper } from "../transformers/LineGrouper";
 import { LineSplitter } from "../transformers/LineSplitter";
 import { Readable } from "node:stream";
+import { TextDecoderStream } from "../polifylls";
 import { pipeline } from "node:stream/promises";
+import { splitInParts } from "../splitInParts";
+import { sql } from "../db";
 
 // prevents TS errors
 declare var self: Worker;
@@ -48,6 +48,8 @@ async function retryToInsert(lines: string[], error: Error) {
 self.onmessage = async (event: MessageEvent<string>) => {
 	console.log("DNI worker started");
 	const dniFilePath = event.data
+
+	console.log("Reading DNI file");
 	const file = Bun.file(dniFilePath)
 	const fileStream = file.stream()
 
@@ -60,6 +62,7 @@ self.onmessage = async (event: MessageEvent<string>) => {
 		.pipeThrough(lineTransformStream)
 		.pipeThrough(lineGroupTransformStream)
 
+	console.log("Inserting DNIs");
 	for await (const lines of dnisStream) {
 		const personaLines: string[] = []
 		for (const line of lines) {
