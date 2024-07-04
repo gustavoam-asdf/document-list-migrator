7600a01a0d0fedf59cec42ef7f3746193935b4fe
Gustavo A. <gustavo.atencio@outlook.com>
2024-07-04 11:41:12 -0500

    update date in both databases

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { primarySql, secondarySql } from "./db";
+
 import { WorkerPromise } from "./WorkerPromise";
 import { updateRucsFile } from "./updateRucsFile";
 
@@ -10,6 +12,56 @@ const {
 	dnisPath,
 	rucsPath,
 } = await updateRucsFile();
+
+const secondaryTimeStart = Date.now();
+
+await secondarySql.begin(async sql => {
+	await sql`TRUNCATE "PersonaNatural"`
+	await sql`TRUNCATE "PersonaJuridica"`
+
+	console.log("Truncated secondary tables");
+})
+
+const secondaryDniWorker = WorkerPromise({
+	path: "./workers/dniWorker.ts",
+	name: "dniWorker",
+	startMessage: {
+		filePath: dnisPath,
+		useSecondaryDb: true,
+	}
+})
+
+const secondaryRucWorker = WorkerPromise({
+	path: "./workers/rucWorker.ts",
+	name: "rucWorker",
+	startMessage: {
+		filePath: rucsPath,
+		useSecondaryDb: true,
+	}
+})
+
+await Promise.all([
+	secondaryDniWorker.then(() => {
+		const endTime = Date.now();
+		console.log(`Done secondary DNI in ${endTime - secondaryTimeStart}ms`);
+	}),
+	secondaryRucWorker.then(() => {
+		const endTime = Date.now();
+		console.log(`Done secondary RUC in ${endTime - secondaryTimeStart}ms`);
+	}),
+])
+
+//Set in redis that is updating
+//
+
+const primaryTimeStart = Date.now();
+
+await primarySql.begin(async sql => {
+	await sql`TRUNCATE "PersonaNatural"`
+	await sql`TRUNCATE "PersonaJuridica"`
+
+	console.log("Truncated primary tables");
+})
 
 const dniWorker = WorkerPromise({
 	path: "./workers/dniWorker.ts",
@@ -32,11 +84,11 @@ const rucWorker = WorkerPromise({
 await Promise.all([
 	dniWorker.then(() => {
 		const endTime = Date.now();
-		console.log(`Done DNI in ${endTime - startTime}ms`);
+		console.log(`Done DNI in ${endTime - primaryTimeStart}ms`);
 	}),
 	rucWorker.then(() => {
 		const endTime = Date.now();
-		console.log(`Done RUC in ${endTime - startTime}ms`);
+		console.log(`Done RUC in ${endTime - primaryTimeStart}ms`);
 	}),
 ])
 
