6a4dea8efcf8cb120abaae30114f9c3996d63d2f
Gustavo A. <gustavo.atencio@outlook.com>
2024-07-04 10:35:51 -0500

    skip line header

--- src/transformers/LineSplitter.ts
+++ src/transformers/LineSplitter.ts
@@ -9,7 +9,16 @@ export class LineSplitter implements Transformer<string, string> {
 		this.buffer += chunk;
 		let lines = this.buffer.split('\n');
 		this.buffer = lines.pop() || ''; // Guardar el último fragmento para la próxima chunk
+		let hasSkippedHeader = false;
 		for (let line of lines) {
+			if (!hasSkippedHeader) {
+				const isHeader = line.startsWith("RUC");
+				if (isHeader) {
+					hasSkippedHeader = true;
+					continue;
+				}
+			}
+
 			controller.enqueue(line);
 		}
 	}
